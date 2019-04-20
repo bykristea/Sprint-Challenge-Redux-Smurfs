43c64b6b7724118280e591296e22704e9e791d6d
Kristea Gamel <krgamel@gmail.com>
2019-04-20 11:35:38 -0500

    acitons to .get array of smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,7 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import axios from 'axios';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +13,16 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_SMURFS_START });
+  axios
+  .get('http://localhost:3333/smurfs')
+  .then(response =>
+    dispatch({ type: FETCH_SMURFS_SUCCESS, payload: response.data.results }))
+    .catch(err => dispatch({type: FETCH_SMURFS_FAIL, payload: err}))
+}
